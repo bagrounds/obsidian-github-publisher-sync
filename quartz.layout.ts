510178776ef361c3fe196f05c7c433129c8883ed
Bryan Grounds <bagrounds@gmail.com>
2024-04-18 09:18:47 -0700

    Update quartz.layout.ts: point GitHub footer link to the specific source repo

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,7 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/bagrounds"
+      GitHub: "https://github.com/bagrounds/obsidian-github-publisher-sync"
     },
   }),
 }

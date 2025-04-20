606fe25ac6b0c3dbab40d8397c43f766cb0404bb
Bryan Grounds <bagrounds@gmail.com>
2025-04-20 15:44:10 -0700

    quartz: conditionally render backlinks

--- quartz/components/Backlinks.tsx
+++ quartz/components/Backlinks.tsx
@@ -23,6 +23,7 @@ export default ((opts?: Partial<BacklinksOptions>) => {
     displayClass,
     cfg,
   }: QuartzComponentProps) => {
+    if (fileData.frontmatter?.backlinks === false) return
     const slug = simplifySlug(fileData.slug!)
     const backlinkFiles = allFiles.filter((file) => file.links?.includes(slug))
     if (options.hideWhenEmpty && backlinkFiles.length == 0) {

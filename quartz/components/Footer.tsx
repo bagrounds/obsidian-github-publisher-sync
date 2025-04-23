94b18fb4d77d375427ed5d0605064995a95c7ce3
Bryan Grounds <bagrounds@gmail.com>
2025-04-23 13:44:32 -0700

    quartz: Update Footer.tsx - powered by...

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,9 +13,8 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+        <p>🔌 Powered by 
+          <a href="https://quartz.jzhao.xyz/">💎 Quartz</a> + <a href="https://obsidian.md">✍️ Obsidian</a> + <a href="https://github.com/ObsidianPublisher/obsidian-github-publisher">📨 Enveloppe</a>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (

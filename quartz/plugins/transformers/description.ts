96e72096d0c6ab51dcb5eaf5b107ffa647c9db2b
Bryan Grounds <bagrounds@gmail.com>
2025-06-04 20:02:51 -0700

    quartz: Update description.ts - fix ?

--- quartz/plugins/transformers/description.ts
+++ quartz/plugins/transformers/description.ts
@@ -58,7 +58,7 @@ export const Description: QuartzTransformerPlugin<Partial<Options>> = (userOpts)
             let text = removeTitle(escapeHTML(toString(tree)), file.data.frontmatter?.title)
             const AFFILIATE_TEXT = 'As an Amazon Associate I earn from qualifying purchases.'
 
-            let affiliateLessText = text.split(AFFILIATE_TEXT).?[1]
+            let affiliateLessText = text.split(AFFILIATE_TEXT)?.[1]
             text = affiliateLessText || text
 
             if (opts.replaceExternalLinks) {
